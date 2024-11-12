0e0e03d2a46060d192ef79c0fbb36b1013b1d6b1
Jiahe <yi.jiahe00@gmail.com>
2024-11-13 00:22:58 +0800

    Send left half for production

--- ergogen/footprints/covered_via.js
+++ ergogen/footprints/covered_via.js
@@ -6,10 +6,13 @@ module.exports = {
   body: p => {
     const [net_index, net_name] = `${p.net}`.slice(5, -1).split(" ");
 
+    // constrain hole to 0.2 - 0.4, with a minimum annular width of 0.1mm 
+    const drill_size = Math.min(0.4, Math.max(size - 1, 0.2));
   return `
     (via
       (at ${p.x} ${p.y})
       (size ${p.size})
+      (drill ${drill_size})
       (layers "F.Cu" "B.Cu")
       (net ${net_index})
     )
