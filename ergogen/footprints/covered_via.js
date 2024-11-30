6d4913f76ece038d1de70ca8164a80a8681f94b5
Jiahe <yi.jiahe00@gmail.com>
2024-11-30 17:07:12 +0800

    fix: Fix via footprint

--- ergogen/footprints/covered_via.js
+++ ergogen/footprints/covered_via.js
@@ -1,13 +1,14 @@
 module.exports = {
   params: {
+    // via diameter in mm
     size: 0.8,
     net: undefined
   },
   body: p => {
     const [net_index, net_name] = `${p.net}`.slice(5, -1).split(" ");
 
-    // constrain hole to 0.2 - 0.4, with a minimum annular width of 0.1mm 
-    const drill_size = Math.min(0.4, Math.max(size - 1, 0.2));
+    // constrain hole to diameter of 0.3 - 0.4mm, with a minimum annular width of 0.1mm 
+    const drill_size = Math.min(0.4, Math.max(p.size - (0.1 * 2), 0.3));
   return `
     (via
       (at ${p.x} ${p.y})
